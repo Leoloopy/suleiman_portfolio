46fb747341cfa5546567aed843e4be8a6fea6e63
Bolaji <abolajiidiisu@gmail.com>
2021-06-06 14:16:33 +0100

    contact form rectified

--- script.js
+++ script.js
@@ -31,3 +31,35 @@ function addScrollEffect(){
 }
 
 
+let fullName = document.getElementById('fullName')
+let contact = document.getElementById('contact')
+// let jobTitle = document.getElementById('jobtitle')
+let message = document.getElementById('msg')
+let submit = document.getElementById('submit')
+
+function validate() {
+submit.addEventListener('click' , (e) => {
+    e.preventDefault();
+    
+if (fullName.value == "" || contact.value == "" || message.value == "") {
+    alert('fill in the required fields')
+} 
+else {
+    sendMail(fullName.value , contact.value, message.value)
+    alert('message sent')
+    fullName.value = ""
+    contact.value = ""
+    message.value = ""
+}
+})
+}
+validate();
+
+function sendMail(name ,email, msg) {
+    emailjs.send("service_5qfgava","template_a80ilkm",{
+        to_name: name,
+        from_name: email,
+        message: msg,
+});
+}
+
